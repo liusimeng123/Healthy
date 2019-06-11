09ed67ac1851b7f72b053766dab4be1c60f678aa
liusimeng123 <160689245@qq.com>
2019-06-11 23:54:04 +0800

    try4

--- app/src/main/java/com/suwfe/sm/healthy/AlarmFragment.java
+++ app/src/main/java/com/suwfe/sm/healthy/AlarmFragment.java
@@ -8,14 +8,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class AlarmFragment extends Fragment {
     @Nullable
     @Override
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container);
+
+        View view = inflater.inflate(R.layout.frame_alarm, null);
+        Button PresentLoc = (Button) view.findViewById(R.id.button_alarm); //此处使得Button和xml中的按钮联系
+        PresentLoc.setOnClickListener(new LocationCheckedListener()); //这一行是在将button和监听器捆绑
+        return view;
+
     }
 
     @Override
@@ -23,4 +32,13 @@ public class AlarmFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    class LocationCheckedListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(getActivity(), MainActivity.class); //从前者跳到后者，特别注意的是，在fragment中，用getActivity()来获取当前的activity
+            getActivity().startActivity(intent);
+        }
+
+    }
 }
--- app/src/main/java/com/suwfe/sm/healthy/PersonalFragment.java
+++ app/src/main/java/com/suwfe/sm/healthy/PersonalFragment.java
@@ -1,26 +1,67 @@
 package com.suwfe.sm.healthy;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PersonalFragment extends Fragment {
+    private final String TAG = "PersonalFragment";
+    private float weight_water_Rate = 40.0f;
+
+    EditText weight;
+    TextView show;
+    TextView question1;
+    TextView question2;
+    float weight2;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frame_personal, container);
+        View view = getActivity().getLayoutInflater().inflate(R.layout.frame_personal, null);
+        question1 = view.findViewById(R.id.personalTextView1);
+        weight = view.findViewById(R.id.personalEditView1);
+        question2 = view.findViewById(R.id.personalTextView2);
+        show = view.findViewById(R.id.personalTextView3);
+
+
+
+        weight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(s.length()>0){
+                    float r = Float.parseFloat(s.toString());
+                    show.setText(String.valueOf(r*weight_water_Rate));
+                }else{
+                    Toast.makeText(getActivity(), "请输入内容", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+       return view;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        TextView tv =(TextView)getView().findViewById(R.id.personalTextView1);
-        tv.setText("personal");
     }
 }
