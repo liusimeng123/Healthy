bf4607445aaa0094a8ac390cd04087fa83a54e4f
liusimeng123 <160689245@qq.com>
2019-06-18 21:36:53 +0800

    try5

--- app/src/main/java/com/suwfe/sm/healthy/PersonalFragment.java
+++ app/src/main/java/com/suwfe/sm/healthy/PersonalFragment.java
@@ -1,8 +1,10 @@
 package com.suwfe.sm.healthy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,10 +14,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonalFragment extends Fragment {
     private final String TAG = "PersonalFragment";
@@ -25,10 +31,12 @@ public class PersonalFragment extends Fragment {
     TextView show;
     TextView question1;
     TextView question2;
-    float weight2;
+    private  float weight2;
+    String str;
 
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.frame_personal, null);
@@ -38,6 +46,11 @@ public class PersonalFragment extends Fragment {
         show = view.findViewById(R.id.personalTextView3);
 
 
+        Button PresentLoc = (Button) view.findViewById(R.id.button); //此处使得Button和xml中的按钮联系
+        PresentLoc.setOnClickListener(new PersonalFragment.LocationCheckedListener()); //这一行是在将button和监听器捆绑
+
+
+        //weight2 = sharedPreferences.getFloat("weight2_rate",40.0f);
 
         weight.addTextChangedListener(new TextWatcher() {
             @Override
@@ -63,12 +76,26 @@ public class PersonalFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Button button = (Button) getActivity().findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "nihao", Toast.LENGTH_LONG).show();
-            }
-        });
+    }
+    class LocationCheckedListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(getActivity(), CountActivity.class); //从前者跳到后者，特别注意的是，在fragment中，用getActivity()来获取当前的activity
+            weight2=Float.parseFloat(weight.getText().toString());
+            intent.putExtra("weight",weight2);
+            Log.i(TAG, "onClick: "+weight2);
+            getActivity().startActivityForResult(intent,1);;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==2){
+
+            Bundle bundle = data.getExtras();
+            weight2 = bundle.getFloat("key_dollar",40.0f);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
